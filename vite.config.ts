bfaddb54bc567684e3a3dbfeeb5373e956988554
youngmoguler <sudhanshushukla902@gmail.com>
2023-07-06 03:03:44 +0530

    Removed registerType from vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,6 @@ export default defineConfig({
 	plugins: [
 		react(),
 		VitePWA({
-			registerType: "autoUpdate",
 			// add this to cache all the imports
 			workbox: {
 				globPatterns: ["**/*"]
