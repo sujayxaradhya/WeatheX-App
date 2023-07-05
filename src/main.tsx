e722c2d52a1dde7acaf3a713946188ff273a24bb
Sujay <51851624+youngmoguler@users.noreply.github.com>
2023-07-05 07:52:25 +0530

    Update main.tsx
    Added registration: serviceWorkerRegistration
    Added error handling in registration

--- src/main.tsx
+++ src/main.tsx
@@ -4,14 +4,18 @@ import { Provider } from "react-redux";
 import { store } from "./app/store.ts";
 import App from "./App.tsx";
 
+let serviceWorkerRegistration = null;
+
 if ("serviceWorker" in navigator) {
 	navigator.serviceWorker
 		.register("/src/utils/serviceWorker.ts")
 		.then((registration) => {
 			// Service worker registered successfully
+			serviceWorkerRegistration = registration;
 		})
 		.catch((error) => {
 			// Service worker registration failed
+			console.error(error);
 		});
 }
 
